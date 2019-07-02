7d12338063619172f79ae7f416779b30317875d7
Shannon Jordan <shannonmj@fb.com>
2019-07-02 16:00:38 -0700

    fully loading app

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,106 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    private List<Tweet> mTweets;
+    Context context;
+    // pass in the Tweets array into the constructor
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+    // for each row, inflate the layout and cache references into viewholder
+    // TODO does the Nonnull matter?
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    // bind the values based on the position of the elements
+    // for when the user scrolls down
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        // get the data according to position
+        Tweet tweet = mTweets.get(position);
+
+        // populate the views according to this data
+        viewHolder.tvUsername.setText(tweet.user.name);
+        viewHolder.tvBody.setText(tweet.body);
+        viewHolder.tvTime.setText(getRelativeTimeAgo(tweet.createdAt));
+
+        //to display profile pictures
+        // also added android:usesCleartextTraffic="true" to manifest in order to clear traffic because andoroid does not support http
+        //other way to do this is with if then statement (if http then make https)
+        Glide.with(context).load(tweet.user.profileImageUrl).into(viewHolder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // create ViewHolder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivProfileImage;
+        private TextView tvUsername;
+        public  TextView tvBody;
+        public TextView tvTime;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            // perform findViewById lookups
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+            tvTime = (TextView) itemView.findViewById(R.id.tvTime);
+        }
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+}
