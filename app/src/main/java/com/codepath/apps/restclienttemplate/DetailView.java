819592ca8ab24c1b476225608fc2e3121cc34776
Shannon Jordan <shannonmj@fb.com>
2019-07-05 18:25:00 -0700

    final project

--- app/src/main/java/com/codepath/apps/restclienttemplate/DetailView.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailView.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,6 +23,7 @@ public class DetailView extends AppCompatActivity {
     public  TextView tvBody;
     public TextView tvTime;
     public TextView tvAtName;
+    public ImageView entityTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +36,12 @@ public class DetailView extends AppCompatActivity {
         tvBody = (TextView) findViewById(R.id.tvBody);
         tvTime = (TextView) findViewById(R.id.tvTime);
         tvAtName = (TextView) findViewById(R.id.tvAtName);
+        entityTweet = (ImageView) findViewById(R.id.entity_tweet);
 
         // unwrap the tweet passed in via intent
         tweet = (Tweet) Parcels.unwrap(getIntent().getParcelableExtra(Tweet.class.getSimpleName()));
         user = (User) Parcels.unwrap(getIntent().getParcelableExtra(User.class.getSimpleName()));
+
 
         // set the title and overview
         tvUsername.setText(tweet.user.getName());
@@ -46,10 +50,20 @@ public class DetailView extends AppCompatActivity {
         tvAtName.setText("@" + tweet.user.getScreenName());
 
 
+
         //load image using glide
         Glide.with(this)
                 .load(tweet.user.profileImageUrl)
                 .into(ivProfileImage);
+
+        if(tweet.hasEntities){
+            String entitiesUrl = tweet.entity.loadURL;
+            entityTweet.setVisibility(View.VISIBLE);
+            Glide.with(this).load(entitiesUrl).into(entityTweet);
+        } else{
+            entityTweet.setVisibility(View.GONE);
+        }
     }
+
 
 }
